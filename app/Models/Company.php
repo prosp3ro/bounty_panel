62259f6a98b0e1c5a36c20dc31fc224b2412eeb4
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2025-06-02 13:56:02 +0200

    application setup

---
+++ app/Models/Company.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $slug
+ * @property string $company_name
+ * @property string|null $logo_path
+ * @property string|null $website_url
+ * @property string|null $contact_url
+ * @property string|null $business_email
+ * @property string|null $country
+ * @property string|null $address
+ * @property string|null $phone_number
+ * @property string|null $about
+ * @property int $auto_join_enabled
+ * @property string $plan
+ * @property string|null $billing_customer_id
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereAbout($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereAutoJoinEnabled($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereBillingCustomerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereBusinessEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereCompanyName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereContactUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereCountry($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereLogoPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company wherePlan($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereWebsiteUrl($value)
+ * @mixin \Eloquent
+ */
+class Company extends Model
+{
+    //
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,53 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $username
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $avatar_path
+ * @property string|null $website_url
+ * @property string|null $phone_number
+ * @property string|null $bio
+ * @property string $password
+ * @property int $is_public
+ * @property int $is_app_admin
+ * @property string|null $remember_token
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereAvatarPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereBio($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereIsAppAdmin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereIsPublic($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUsername($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereWebsiteUrl($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
