a6c7d11b6e03f707c0b03e9b62bd741aa72dc3a0
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2025-06-02 13:59:32 +0200

    application setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::shouldBeStrict(!App::isProduction());
+        DB::prohibitDestructiveCommands(App::isProduction());
     }
 }
